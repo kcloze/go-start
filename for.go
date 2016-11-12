5f1c5ca3cd9901eed9b57e40205a171f2af0be69
kcloze <lixiaopei@yaochufa.com>
2016-11-12 19:00:00 +0800

    go编程练习

---
+++ for.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	i := 1
+	for i <= 3 {
+		fmt.Println(i)
+		i = i + 1
+	}
+	for j := 1; j < 10; j++ {
+		fmt.Println(j)
+	}
+	for {
+		fmt.Println("loop")
+		break
+	}
+}
